f4bb737e9dbb306bafa501411b0c80fa1956e100
DwayneWard <neonyan15@gmail.com>
2022-03-07 14:19:44 +0300

    initial commit

---
+++ run.py
@@ -0,0 +1,19 @@
+from project.config import DevelopmentConfig
+from project.dao.models import Genre
+from project.dao.models.director import Director
+from project.dao.models.movie import Movie
+from project.dao.models.user import User
+from project.server import create_app, db
+
+app = create_app(DevelopmentConfig)
+
+
+@app.shell_context_processor
+def shell():
+    return {
+        "db": db,
+        "Genre": Genre,
+        "Director": Director,
+        "Movie": Movie,
+        "User": User
+    }
